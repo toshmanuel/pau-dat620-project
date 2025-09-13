6493f10ffa3970d9316b1499711729a7ce3684bb
toshmanuel <oladimejiomotosho5@gmail.com>
2025-09-13 17:29:27 +0100

    updated code

--- contracts/DimsToken.sol
+++ contracts/DimsToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.30;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DimsToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("DimsToken", "DTK") {
+    constructor(uint256 initialSupply) ERC20("DimsToken", "DIMS") {
         _mint(msg.sender, initialSupply * 10 ** decimals());  // Mint total supply to deployer
     }
 }
